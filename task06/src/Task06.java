00a0c6a8a4e668346b9064c222c6b3fa63fb7eac
ZhekaProff <Zhekakad@ya.ru>
2022-09-20 00:13:35 +0900

    Реализовать алгоритм сортировки слиянием

---
+++ task06/src/Task06.java
@@ -0,0 +1,69 @@
+/*
+Задача: Реализовать алгоритм сортировки слиянием
+ */
+
+public class Task06 {
+    private static String arrayToString(int[] array) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < array.length; i++) {
+            if (i > 0) {
+                sb.append(", ");
+            }
+            sb.append(array[i]);
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+    public static int[] mergeSort(int[] array) {
+        int[] tmp;
+        int[] currentSrc = array;
+        int[] currentDest = new int[array.length];
+
+        int size = 1;
+        while (size < array.length) {
+            for (int i = 0; i < array.length; i += 2 * size) {
+                merge(currentSrc, i, currentSrc, i + size, currentDest, i, size);
+            }
+            tmp = currentSrc;
+            currentSrc = currentDest;
+            currentDest = tmp;
+            size = size * 2;
+        }
+        return currentSrc;
+    }
+    private static void merge(int[] src1, int src1Start, int[] src2, int src2Start, int[] dest,
+                              int destStart, int size){
+        int index1 = src1Start;
+        int index2 = src2Start;
+
+        int src1End = Math.min(src1Start + size, src1.length);
+        int src2End = Math.min(src2Start + size, src2.length);
+
+        if (src1Start + size > src1.length) {
+            for (int i = src1Start; i < src1End; i++) {
+                dest[i] = src1[i];
+            }
+            return;
+        }
+        int iterationCount = src1End - src1Start + src2End - src2Start;
+
+        for (int i = destStart; i < destStart + iterationCount; i++) {
+            if (index1 < src1End && (index2 >= src2End || src1[index1] < src2[index2])) {
+                dest[i] = src1[index1];
+                index1++;
+            } else {
+                dest[i] = src2[index2];
+                index2++;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] array = new int[] {15,78,89,9,12};
+        System.out.println("Начальный массив: " + arrayToString(array));
+        array = mergeSort(array);
+        System.out.println("Конечный массив:  " + arrayToString(array));
+    }
+}
