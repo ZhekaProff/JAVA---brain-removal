901ea0ac5a51d4e8c28c2a9bb7a58a16336e7c00
ZhekaProff <Zhekakad@ya.ru>
2022-10-31 22:13:36 +0900

    7 дз на пайтон

---
+++ OOPDZ07/ExampleApp.py
@@ -0,0 +1,74 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QObject, Qt
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtWidgets import QMessageBox
+import threading
+from processor import Processor
+import threading
+import requests
+import init
+import sys
+import os
+import untitled
+
+
+
+
+class ExampleApp(QtWidgets.QMainWindow, untitled.Ui_MainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.setupUi(self)
+
+        self.btn_select.clicked.connect(self.select)
+        self.btn_check.clicked.connect(self.check)
+        self.btn_download.clicked.connect(self.download)
+
+    def select(self):
+
+        self.file = QtWidgets.QFileDialog.getExistingDirectory(self, "Select file")
+        if self.file != "":
+            self.line_save.setText(self.file)
+        else:
+            pass
+
+    def check(self):  # Старт видео
+        self.title = self.line_title.text()
+        self.url = self.line_url.text()
+        self.path = self.line_save.text()
+        self.th = Processor(self.title, self.url, self.path)
+        threading.Thread(target=self.th.running).start()
+        self.th.length.connect(self.append)
+
+    def download(self):  # Старт загрузки
+        self.title = self.line_title.text()
+        self.url = self.line_url.text()
+        self.path = self.line_save.text()
+        self.th = Processor(self.title, self.url, self.path)
+        threading.Thread(target=self.th.download).start()
+        self.th.receiv.connect(self.progress)
+
+    def append(self, title, description, thumbnail):  # Start stream metod...
+
+        if title != "":
+            self.line_title.setText(title)
+            self.text_desc.setPlainText(description)
+            image = QImage()
+            image.loadFromData(requests.get(thumbnail).content)
+            self.label_prew.setPixmap(QPixmap(image))
+            self.label_prew.show()
+        else:
+            self.msg()
+
+    def progress(self, size):  # Прогресс бар
+        self.progressBar.setValue(size)
+
+    def msg(self):
+        dialog = QMessageBox()
+        dialog.setText("Проверьте правильность ссылки!")
+        dialog.setInformativeText("<i>Скопируйте ссылку на видео из строки браузера...</i>")
+        dialog.setWindowTitle("Видео недоступно")
+        dialog.setStandardButtons(QMessageBox.Cancel)
+        dialog.setWindowFlags(Qt.CustomizeWindowHint | Qt.WindowTitleHint)
+        dialog.exec_()
---
+++ OOPDZ07/processor.py
@@ -0,0 +1,62 @@
+
+from __future__ import unicode_literals
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QObject
+from pytube import YouTube
+from pytube import Stream
+import youtube_dl
+import os
+
+
+class Processor(QObject):
+    length = QtCore.pyqtSignal(str, str, str)
+    receiv = QtCore.pyqtSignal(int)
+
+    def __init__(self, title=None, url=None, path=None):
+        super().__init__()
+
+        self.title = title
+        self.url = url
+        self.path = path
+
+    def running(self):
+        dl = youtube_dl.YoutubeDL()
+        try:
+            result = dl.extract_info(self.url, download=False)
+
+            if 'entries' in result:
+                video = result['entries'][0]
+            else:
+                video = result
+
+            video_title = video['title']
+            video_description = video['description']
+            video_thumbnail = video['thumbnail']
+            video_url = video['webpage_url']
+
+            self.length.emit(video_title, video_description, video_thumbnail)
+            print(video_thumbnail)
+        except Exception as error:
+            self.length.emit(None, None, None)
+            print(error)
+
+    def download(self):
+        dl_options = {'format': 'best', 'outtmpl': self.path + "/" + self.title + ".mp4",
+                      'progress_hooks': [self.progress]}
+        with youtube_dl.YoutubeDL(dl_options) as dl:
+            dl.download([self.url])
+
+    def progress(self, percent):
+        if percent['status'] == 'downloading':
+            result = round(percent['downloaded_bytes'] / percent['total_bytes'] * 100, 1)
+
+            print(round(percent['downloaded_bytes'] / percent['total_bytes'] * 100, 1))
+            self.receiv.emit(result)
+
+    def on_progress(self, stream: Stream, chunk: bytes, bytes_remaining: int) -> None:
+
+        filesize = stream.filesize
+        bytes_received = filesize - bytes_remaining
+        percent = round(100.0 * bytes_received / float(filesize), 1)
+        self.receiv.emit(percent)
+        print(bytes_received, filesize)
